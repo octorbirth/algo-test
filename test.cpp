87fc448baadd40f462c48d428c48739116b5daaf
harbang <hsu9643@naver.com>
2021-04-10 12:33:13 +0900

    push test

--- test.cpp
+++ test.cpp
@@ -2,12 +2,4 @@
 
 int main(){
     printf("(A) Push Remote..");
-    printf("(B) Commit (Not Push)..");
-    printf("(C) Add (Not Commit)..");
-    printf("(te Add (Not Commit)..");
-    printf("(C) Add (Not Commit)..");
 }
-
-dd
-
-
